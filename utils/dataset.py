e5a5c4bee1284114cdb1600424e00e9a651055ce
Shi Chen <shichen.py@gmail.com>
2022-07-19 09:02:38 +0900

    feat: add coco to labelme function (not debugging yet).

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,4 +1,5 @@
-
+import numpy as np
+from skimage.measure import find_contours
 from utils.general import random_index
 
 def get_dataset_type(split_ratio):
@@ -36,3 +37,44 @@ def get_category_ids(cat_file, no_background=True):
         cat_ids[cat_name] = cat_id
 
     return cat_ids
+
+def coco2shape(row):
+    if row.iscrowd == 1:
+        shapes = rle2shape(row)
+    elif row.iscrowd == 0:
+        shapes = polygon2shape(row)
+    return shapes
+
+def rle2shape(row):
+    rle, shape = row['segmentation']['counts'], row['segmentation']['size']
+    mask = rle_decode(rle, shape)
+    padded_mask = np.zeros((mask.shape[0]+2, mask.shape[1]+2),
+                            dtype=np.uint8,)
+    padded_mask[1:-1, 1:-1] = mask
+    points = find_contours(mask, 0.5)
+    shapes = [[[int(point[1]), int(point[0])] for point in polygon]
+              for polygon in points]
+    return shapes
+
+def rle_decode(rle, shape):
+    mask = np.zeros([shape[0] * shape[1]], np.bool)
+    for idx, r in enumerate(rle):
+        s = 0 if idx < 1 else sum(rle[:idx])
+        e = s + r
+        if e == s:
+            continue
+        assert 0 <= s < mask.shape[0]
+        assert 1 <= e <= mask.shape[0], 'shape: {}  s {}  e {} r {}'.format(shape, s, e, r)
+        if idx % 2 == 1:
+            mask[s:e] = 1
+
+    # Reshape and transpose
+    mask = mask.reshape([shape[1], shape[0]]).T
+    return mask
+
+def polygon2shape(row):
+    # shapes: (n_polygons, n_points, 2)
+    shapes = [[[int(points[2*i]), int(points[2*i+1])] for i in range(len(points)//2)]
+              for points in row.segmentation]
+
+    return shapes
